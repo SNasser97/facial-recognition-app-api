830e1d7e0b9c9cf6263f7c98011c80d11c703d18
SNasser97 <snasserahmed@outlook.com>
2020-02-27 20:52:21 +0000

    destructure clarifai api data response

--- controllers/profile.js
+++ controllers/profile.js
@@ -8,7 +8,7 @@ const handleApiCall = (req, res) => {
 	// console.log(req.body.input);
 	app.models
 		.predict(FACE_DETECT_MODEL, req.body.input)
-		.then(data => res.json(data)
+		.then(data => res.json({ data }))
 		.catch(err => res.status(400).json("Unable to work with api"));
 }
 
