d355e08bc84f16601455e65478d3f53e7df92709
SNasser97 <snasserahmed@outlook.com>
2020-02-28 15:04:00 +0000

    add env variable from heroku for api key

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,7 @@
 const { App, FACE_DETECT_MODEL } = require('clarifai');
 
 const app = new Clarifai.App({
-	apiKey: "78172c5a96694b898b0c5284741dbcd9" // ADD YOUR API KEY HERE
+	apiKey: process.env.API_CLARIFAI_KEY // ADD YOUR API KEY HERE
 });
 
 const handleApiCall = (req, res) => {
